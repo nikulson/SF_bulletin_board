a9de1f388047dd4f26c41ed871ece9a6e57e7999
nikulson <nikulshin@mail.ru>
2022-12-01 00:44:59 +0400

    first models, views

--- bulletin_board/callboard/urls.py
+++ bulletin_board/callboard/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import BulletinListView, UserBulletinListView
+from .views import BulletinListView, UserBulletinListView, ReplyListView
 
 urlpatterns = [
     path('', BulletinListView.as_view(), name='home'),
     path('user/<str:username>', UserBulletinListView.as_view(), name='user-posts'),
-
+    path('home/replies', ReplyListView.as_view(), name='reply_list'),
 
 ]
--- bulletin_board/callboard/views.py
+++ bulletin_board/callboard/views.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.views.generic.base import View, TemplateView
 
-from .filters import BulletinFilter
-from .forms import BulletinForm
-from .models import Bulletin, Category
+from .filters import BulletinFilter, ReplyFilter
+from .forms import BulletinForm, ReplyForm
+from .models import Bulletin, Category, Reply
 
 
 def home(request):
@@ -16,10 +18,11 @@ def home(request):
     }
     return render(request, 'bulletins/home.html', context)
 
+
 class BulletinListView(ListView):
     model = Bulletin
     queryset = Bulletin.objects.all()
-    template_name = "bulletins/bulletin_list.html"
+    template_name = "bulletins/home.html"
     context_object_name = 'bulletin_list'
     ordering = ['-created_at']
     paginate_by = 2
@@ -57,8 +60,74 @@ class BulletinDetail(DetailView):
     model = Bulletin
 
 
+class BulletinCreateView(LoginRequiredMixin, CreateView):
+    model = Bulletin
+    fields = ['title', 'content', 'category', 'file']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('home')
 
 
+class ReplyCreateView(LoginRequiredMixin, CreateView):
+    model = Reply
+    form_class = ReplyForm
+
+    def form_valid(self, form):
+        form.instance.bulletin_id = self.kwargs['pk']
+        form.instance.username = self.request.user
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('home')
 
 
+class BulletinUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Bulletin
+    fields = ['title', 'body', 'category']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        bulletin = self.get_object()
+        if self.request.user == bulletin.author:
+            return True
+        return False
+
+
+class BulletinDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Bulletin
+    success_url = '/home'
+
+    def test_func(self):
+        bulletin = self.get_object()
+
+        if self.request.user == bulletin.author:
+            return True
+        return False
+
+
+class ReplyListView(LoginRequiredMixin, ListView):
+    model = Reply
+    template_name = 'bulletins/reply_list.html'
+    context_object_name = 'replies'
+    ordering = ['-created_at']
+    paginate_by = 2
+    myFilter = ReplyFilter()
+
+    def get_queryset(self):
+        user_id = self.request.user.id  # get logged-in user id because he is an author of his own posts
+        return Reply.objects.filter(bulletin__author_id=user_id).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = ReplyFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class ReplyDeleteView(LoginRequiredMixin, DeleteView):
+    model = Reply
+    success_url = reverse_lazy('reply_list')
